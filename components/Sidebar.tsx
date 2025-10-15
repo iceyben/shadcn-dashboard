376e1b4a630920d12e775ccfec949aeb947ab851
Benjamin James Mulbah <mulbahb997@gmail.com>
2025-10-15 11:53:20 +0200

    I have added menu list to the sidebar successfully

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -2,69 +2,54 @@ import React from "react";
 import { UserItem } from "./UserItem";
 import {
   Command,
-  CommandDialog,
-  CommandEmpty,
   CommandGroup,
-  CommandInput,
   CommandItem,
   CommandList,
   CommandSeparator,
-  CommandShortcut,
 } from "@/components/ui/command";
 
 export const Sidebar = () => {
-
   const menuList = [
     {
-      Group: "General",
+      group: "General",
       items: [
-       {
-       
-        items: [
-          {
-            link: "/",
-            text: "Profile",
-          },
-          {
-            link: "/",
-            text: "Inbox",
-          },
-          {
-            link: "/",
-            text: "Billing",
-          },
-          {
-            link: "/",
-            text: "Notifications",
-          },
-        ]
-       }
+        {
+          link: "/",
+          text: "Profile",
+        },
+        {
+          link: "/",
+          text: "Inbox",
+        },
+        {
+          link: "/",
+          text: "Billing",
+        },
+        {
+          link: "/",
+          text: "Notifications",
+        },
       ],
     },
-     {
-      Group: "Settings",
+    {
+      group: "Settings",
       items: [
-       {
-        
-        items: [
-          {
-            link: "/",
-            text: "General Settings",
-          },
-          {
-            link: "/",
-            text: "Privacy",
-          },
-         
-          {
-            link: "/",
-            text: "Logs",
-          }
-        ]
-       }
+        {
+          link: "/",
+          text: "General Settings",
+        },
+        {
+          link: "/",
+          text: "Privacy",
+        },
+        {
+          link: "/",
+          text: "Logs",
+        },
       ],
-    }
-  ]
+    },
+  ];
+
   return (
     <div className="flex flex-col w-[300px] min-w-[300px] border-r min-h-screen">
       <div>
@@ -73,12 +58,16 @@ export const Sidebar = () => {
       <div className="grow">
         <Command>
           <CommandList>
-            <CommandGroup heading="Suggestions">
-              <CommandItem>Calendar</CommandItem>
-              <CommandItem>Search Emoji</CommandItem>
-              <CommandItem>Calculator</CommandItem>
-            </CommandGroup>
-            <CommandSeparator />
+            {menuList.map((menu) => (
+              <React.Fragment key={menu.group}>
+                <CommandGroup heading={menu.group}>
+                  {menu.items.map((item) => (
+                    <CommandItem key={item.text}>{item.text}</CommandItem>
+                  ))}
+                </CommandGroup>
+                <CommandSeparator />
+              </React.Fragment>
+            ))}
           </CommandList>
         </Command>
       </div>
